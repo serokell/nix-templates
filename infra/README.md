d5771a9dd7748a7bea0dcdceed11aa335a04444a
Alexander Bantyev <balsoft@balsoft.ru>
2021-08-09 16:18:42 +0300

    Add licensing information

--- infra/README.md
+++ infra/README.md
@@ -1,3 +1,7 @@
+<!-- SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/> -->
+
+<!-- SPDX-License-Identifier: CC0-1.0 -->
+
 <!-- # Constellation: Cluster Usage -->
 
 <!-- 
--- infra/servers/example/default.nix
+++ infra/servers/example/default.nix
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: CC0-1.0
+
 { config, pkgs, lib, ... }:
 # { }
 throw "Put server config here, and don't forget to rename the directory!"
